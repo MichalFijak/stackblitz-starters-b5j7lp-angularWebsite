12273f63fbf5316e82d9684e3f518ee2d852b9b2
MichalFijak <michalfijak1@hotmail.com>
2024-10-05 19:56:23 +0200

    addded submit for authentication

--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -7,18 +8,22 @@ import { FormsModule } from '@angular/forms';
   template: `
   <main>
     <h2>Enter your credentials</h2>
-    <input type="text" placeholder="Login" [(ngModel)]="login" />
-    <input type="text" placeholder="Password" [(ngModel)]="password"/>
-    <button (click)="onSubmit()">Submit</button>
+    <form #authForm="ngForm" (ngSubmit)="onSubmit(authForm)" novalidate>
+      <input type="text" placeholder="Login" name="login" ngModel required />
+      <input type="text" placeholder="Password" name="password" ngModel required />
+      <button type="submit" [disabled]="!authForm.valid">Submit</button>
+    </form>
   </main>
-`,
+  `,
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  public login: string = '';
-  public password: string = '';
 
-  public onSubmit() {
-    console.log(this.login, this.password);
+  public onSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log(form.value);
+    } else {
+      console.log('Form is invalid');
+    }
   }
 }
