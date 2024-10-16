bb7344a4fb9698a21110e117a840d627e4a01f62
MichalFijak <michalfijak1@hotmail.com>
2024-10-16 19:11:52 +0200

    created service

--- src/shop/cart-sneakpeak/cart-sneakpeak.component.ts
+++ src/shop/cart-sneakpeak/cart-sneakpeak.component.ts
@@ -1,14 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ItemModel } from '../ItemModel';
+import { Subscription } from 'rxjs';
+import { ShopListService } from '../shopList.service';
 
 @Component({
   selector: 'app-cart-sneakpeak',
   standalone: true,
   imports: [],
   templateUrl: './cart-sneakpeak.component.html',
-  styleUrl: './cart-sneakpeak.component.css'
+  styleUrls: ['./cart-sneakpeak.component.css']
 })
-export class CartSneakpeakComponent {
-  @Input() shopCart:ItemModel[]=[];
+export class CartSneakpeakComponent implements OnInit, OnDestroy {
+  private shopListSubscription!: Subscription;
+  protected shopCart: ItemModel[] = [];
 
+  constructor(private shopListService: ShopListService) {}
+
+  ngOnInit(): void {
+    this.shopListSubscription = this.shopListService.getItems().subscribe((items) => {
+      this.shopCart = items;
+      console.log(this.shopCart, 'CartSneakpeakComponent items');
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.shopListSubscription) {
+      this.shopListSubscription.unsubscribe();
+    }
+  }
 }
--- src/shop/shop.component.ts
+++ src/shop/shop.component.ts
@@ -1,28 +1,40 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { ItemModel } from './ItemModel';
 import { CartSneakpeakComponent } from './cart-sneakpeak/cart-sneakpeak.component';
+import { Subscription } from 'rxjs';
+import { ShopListService } from './shopList.service';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [FormsModule,CommonModule,CartSneakpeakComponent],
+  imports: [FormsModule, CommonModule, CartSneakpeakComponent],
   templateUrl: './shop.component.html',
-  styleUrl: './shop.component.css'
+  styleUrls: ['./shop.component.css']
 })
-export class ShopComponent {
+export class ShopComponent implements OnDestroy {
+  protected showCart = false;
+  private shopListSubscription!: Subscription;
 
-  protected shopCart:ItemModel[]=[];
-  protected showCart=false;
-  onAddItem(form:NgForm)
-  {
-    this.shopCart.push(new ItemModel(form.value.name,+form.value.amount))
+  constructor(private shopList: ShopListService) {
+    this.shopListSubscription = this.shopList.getItems().subscribe((items: ItemModel[]) => {
+      console.log(items, 'ShopComponent items');
+    });
   }
 
-  cartSneakpeek()
-  {
-    this.showCart=!this.showCart;
+  ngOnDestroy(): void {
+    if (this.shopListSubscription) {
+      this.shopListSubscription.unsubscribe();
+    }
+  }
+
+  onAddItem(form: NgForm) {
+    this.shopList.addItem(new ItemModel(form.value.name, +form.value.amount));
+    form.reset();
+  }
+
+  cartSneakpeek() {
+    this.showCart = !this.showCart;
   }
 }
-
